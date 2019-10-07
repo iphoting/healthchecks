1bad3b548723cd087fdb2ecec215fa8ce90ab5d5
Ronald Ip <myself@iphoting.com>
2019-10-07 22:37:20 +0800

    Fix breakage from Whitenoise v4.

--- hc/heroku_settings.py
+++ hc/heroku_settings.py
@@ -15,6 +15,18 @@ else:
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
+MIDDLEWARE = (
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "hc.accounts.middleware.TeamAccessMiddleware",
+)
+
 import sys
 LOGGING = {
     'version': 1,
