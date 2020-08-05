50c03f84beeedcf3d67b6b74950b80f002c3877d
Ronald Ip <myself@iphoting.com>
2020-08-05 21:56:36 +0800

    Rebased for upstream v1.16.0.

--- hc/heroku_settings.py
+++ hc/heroku_settings.py
@@ -22,6 +22,7 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "hc.accounts.middleware.TeamAccessMiddleware",
 )
 
